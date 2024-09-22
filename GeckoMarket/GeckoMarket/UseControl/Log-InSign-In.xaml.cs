c2ff15ec47a344196a8237524166f361f8a5a53e
Hansola1 <reynasio27@gmail.com>
2024-09-22 21:09:52 +0700

    1.3

--- GeckoMarket/GeckoMarket/UseControl/Log-InSign-In.xaml.cs
+++ GeckoMarket/GeckoMarket/UseControl/Log-InSign-In.xaml.cs
@@ -1,5 +1,4 @@
-﻿using GeckoMarket.UseControl;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,11 +13,11 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace GeckoMarket
+namespace GeckoMarket.UseControl
 {
-    public partial class MainWindow : Window
+    public partial class Log_InSign_In : UserControl
     {
-        public MainWindow()
+        public Log_InSign_In()
         {
             InitializeComponent();
         }
